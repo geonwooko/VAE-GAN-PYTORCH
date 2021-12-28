6470070c2ca58e502e51f980776d3ee1e8c97b59
Geonwoo Ko <67784610+geonwooko@users.noreply.github.com>
2021-12-28 20:21:36 +0900

    Modify error

--- src/utils.py
+++ src/utils.py
@@ -58,11 +58,11 @@ def find_latent_space_and_show(model, DataLoader, data_root, num_show_images):
         attr_latent_axis[attr] = (latent_axis, sign)
 
         attr_changed_latent = img_latent.clone()
-        attr_changed_latent[:, latent_axis] += (sign * 100)
+        attr_changed_latent += (sign * 100)
         print(latent_axis, sign, (attr_changed_latent[:, latent_axis] - img_latent[:, latent_axis]).sum())
         # print(f"raw : {img_latent}, changed : {attr_changed_latent}")
         changed_img = model.decoder(attr_changed_latent).unsqueeze(dim=1)
-        img = torch.cat([img, torch.zeros_like(changed_img, device=device)], dim=1)
+        img = torch.cat([img, changed_img], dim=1)
         print(img.shape, (changed_img - img_recon.unsqueeze(1)).sum())
 
     img = torch.permute(img, (0, 1, 3, 4, 2)).cpu().detach().numpy()
